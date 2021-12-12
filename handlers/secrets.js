bb61e7551707bb06c1d9ae8184793358af3583df
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2021-12-12 17:31:32 +0600

    Update secrets.js

--- handlers/secrets.js
+++ handlers/secrets.js
@@ -1,5 +1,5 @@
 module.exports = (bot) => {
     bot.variables({
-   TOPGG_AUTH: ""
+   TOPGG_AUTH: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg3Mjg0NDAyMDQxNjY3NTg5MiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjM1MjU2NDc0fQ.ee2P8jtR5zWjJMwxF1oa5qjBncksu150t4LoaPyOAO8"
        })
  }
