4e26440c341e159b1459d9e102a3054b3a6585e8
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2021-12-12 17:32:22 +0600

    Update variables.js

--- handlers/variables.js
+++ handlers/variables.js
@@ -6,7 +6,6 @@ module.exports = (bot) => {
     inv: 'https://discord.com/oauth2/authorize?scope=bot+applications.commands&client_id=872844020416675892&permissions=37088600',
     vote: 'https://top.gg/bot/872844020416675892/vote',
     sserver: 'https://discord.gg/MscZrQq7eW',
-    authkey: 'Token',
     played: '',
     last: 'null',
        })
