71c9443a79a07165e56a45d70c15e1d2c7d450f8
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2021-12-13 15:31:53 +0600

    Update invite.js

--- src/commands/misc/invite.js
+++ src/commands/misc/invite.js
@@ -2,6 +2,6 @@ module.exports = ({
     name: "invite",
     aliases: ["inv"],
     code: `👌 Use this url to invite me to your server: https://sayubot.xyz/invite
-    $channelSendMessage[$getVar[commandlog];$username#$discriminator[$authorID] (\`$authorID\`) used \`INVITE\` command at $serverName (\`$guildID\`) in <#$channelID> (\`$channelID\`)]
+    $channelSendMessage[$getVar[messageChannel];$username#$discriminator[$authorID] (\`$authorID\`) used \`INVITE\` command at $serverName (\`$guildID\`) in <#$channelID> (\`$channelID\`)]
     `
 })
