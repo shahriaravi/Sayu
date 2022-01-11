2962f26a40fb01b9f463c220eb315702bb708493
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-12 00:04:10 +0600

    Delete serverlist.js

--- src/commands/dev/serverlist.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "serverlist",
-    code: `
-        $title[Server List with Invites]
-        $description[$map[$serverIDs[,];,;ac;\n]]
-        $onlyForIDs[758726005174173696;610804106344529931;]
-        `
-}
