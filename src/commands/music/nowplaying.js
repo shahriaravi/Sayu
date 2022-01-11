5eb356bef4560b7ada607e5c6377e512d4fda680
Manjiro Sano <72520032+ManjiroSano@users.noreply.github.com>
2022-01-11 17:09:05 +0600

    Update nowplaying.js

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -5,6 +5,7 @@ const current1 = `$replaceText[$replaceText[$splitText[3];(;];);]$textSplit[$son
 
 module.exports = ({
  name: "nowplaying",
+ $if: "v4",
  aliases: ["np"],
  code: `
 $thumbnail[$songInfo[thumbnail]]
