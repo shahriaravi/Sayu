5f2318aa8a0afc30460212f3909f9001cb88b0e5
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-11 23:43:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Aoijs = require("aoi.js")
-const bot = new Aoijs.Bot(require("./config.json"))
+const bot = new Aoijs.Bot(require("./config/config.json"))
 
 bot.loadCommands(`./src/`)
 
+//configs
+require('./config/channels')(bot)
+require('./config/secrets')(bot)
+
+//handlers
 require('./handlers/variables')(bot)
 require('./handlers/callbacks')(bot)
 require('./handlers/responses')(bot)
-require('./handlers/channels')(bot)
 require('./handlers/status')(bot)
-require('./handlers/secrets')(bot)
