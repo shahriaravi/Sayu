ad6348fda2bcbe35217d04b9f1466768efba457f
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-11 23:35:31 +0600

    Create guildLeft.js

---
+++ src/events/guild/guildLeft.js
@@ -0,0 +1,15 @@
+module.exports = {
+    type: "botLeaveCommand",
+      channel: "$getVar[guildChannel]",
+      code: `
+     $title[$getVar[name] has been removed from a server.]
+     $thumbnail[$serverIcon]
+     $addfield[**Member Count**;$memberscount]
+     $addField[**Guild ID**;\`$guildID\`]
+     $addField[**Server Owner**;$userTag[$ownerID] <@$ownerID> (\`$ownerID\`)]
+     $addField[**Server Name**;$serverName]
+     $color[RED]
+     $footer[Total guilds count: $serverCount]
+     $addTimestamp
+      `
+    }
