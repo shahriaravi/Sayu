4351ad74cbc3c18f12182dcd572be550660322b2
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-13 13:53:38 +0600

    updated to v5

--- src/commands/misc/uptime.js
+++ src/commands/misc/uptime.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'uptime',
     code: 
     `
-    I have been online for: $client[readytimestamp].
+    I have been online for: $djsEval["$uptime[humanize]".split(" ").slice(0,-1).join(" ");yes].
     `
   }
