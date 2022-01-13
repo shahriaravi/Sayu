390c399866bd34e7141fca819fd7a6c306d816ed
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-13 13:45:19 +0600

    updated to v5

--- src/commands/misc/stats.js
+++ src/commands/misc/stats.js
@@ -2,13 +2,11 @@ module.exports = {
     name: 'info',
     aliases: ["botinfo", "about", "stats"],
     code: `
-$thumbnail[$userAvatar[$clientid]]
-  $color[$getVar[color]]
-  $title[Sayu's Statistics]
-  $description[**Uptime:** \`$client[readytimestamp]\`\n**Memory Usage:** \`$ram\`\n**CPU Usage:** \`$cpu%\`\n**Guilds:** \`$serverCount\`\n**Channels:** \`$allChannelsCount\`\n**Songs Played:** \`$getVar[played]\`\n**Discord.js Version:** \`v13.2.0\`\n**Node Version**: \`$nodeVersion\`\n**Sayu Version:** \`v3.6.0\`\n[Invite]($getVar[inv]) | [Vote]($getVar[vote]) | [Support]($getVar[sserver]) | [Creavite.co](https://creavite.co)]
-  $footer[Requested by $userTag;$authorAvatar]
-  $addTimestamp
+$thumbnail[1;$userAvatar[$clientid]]
+  $color[1;$getVar[color]]
+  $title[1;$getVar[name]'s Statistics]
+  $description[1;**Uptime:** \`$djsEval["$uptime[humanize]".split(" ").slice(0,-1).join(" ");yes]\`\n**Memory Usage:** \`$roundTenth[$ram;2]\`\n**CPU Usage:** \`$cpu%\`\n**Guilds:** \`$serverCount\`\n**Channels:** \`$allChannelsCount\`\n**Songs Played:** \`$getVar[played]\`\n**Discord.js Version:** \`v13.2.0\`\n**Node Version**: \`$nodeVersion\`\n**Sayu Version:** \`v3.2.0\` \n[Invite]($getVar[inv]) | [Vote]($getVar[vote]) | [Support]($getVar[sserver])]
+  $footer[1;Requested by $userTag;$authorAvatar]
+  $addTimeStamp[1]
   `
   }
-
-  
