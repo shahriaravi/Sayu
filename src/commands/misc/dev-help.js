18ff45ff5c052cd0aec646b39fd3588a2eb41053
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-13 13:40:40 +0600

    Update dev-help.js

--- src/commands/misc/dev-help.js
+++ src/commands/misc/dev-help.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: "dev",
-    code: `$color[$getVar[color]]
-   $description[
-$addField[Dev Commands;
+    code: `$color[1;$getVar[color]]
+   $description[1;
+$addField[1;Dev Commands;
 \`djseval\`, \`eval\`, \`exec\`, \`reboot\`, \`reload\`, \`send\`, \`shutdown\`, \`update\`]]
    $onlyForIDs[758726005174173696;610804106344529931;]
    $addCmdReactions[👌]
