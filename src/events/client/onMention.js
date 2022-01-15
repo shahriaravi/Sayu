45e4a437bb20e691a121d9748ae74f7ae5fb96b8
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-15 18:51:01 +0600

    Update onMention.js

--- src/events/client/onMention.js
+++ src/events/client/onMention.js
@@ -3,16 +3,16 @@ module.exports = {
     nonPrefixed: true,
     code: `
     $onlyIf[$mentioned[1]==$clientID;]
-    $color[1;$getVar[color]]
-    $thumbnail[1;$userAvatar[$clientID]]
-    $title[1;$getVar[name] - High Quality Music]
-    $description[1;
+    $color[$getVar[color]]
+    $thumbnail[$userAvatar[$clientID]]
+    $title[$getVar[name] - High Quality Music]
+    $description[
 **• Prefix:** \`$getServerVar[prefix]\`
 **• Set New Prefix:** \`$getServerVar[prefix]prefix <new prefix>\`
     
 Join a voice channel and \`$getServerVar[prefix]play\` a song.
 Type \`$getServerVar[prefix]help\` for the list of commands.
-[Invite]($getVar[inv]) | [Vote]($getVar[vote]) | [Support]($getVar[sserver])]
-$footer[1;Latency: $ping ms | Uptime: $djsEval["$uptime[humanize]".split(" ").slice(0,-1).join(" ");yes]]
+[Invite]($getVar[inv]) | [Vote]($getVar[vote]) | [Support]($getVar[sserver]) | [Creavite.co](https://creavite.co)]
+$footer[Latency: $ping ms | Uptime: $client[readytimestamp]]
 $onlyIf[$message[2]==;]`
-   };
+   };  
