951cdec84077e01a1e5578adf30fc3e7c5484a2f
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-15 18:48:20 +0600

    Update help.js

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -1,27 +1,27 @@
 module.exports = {
     name: "help",
-    code: `$color[1;$getVar[color]]
-$author[1;Commands;$userAvatar[$clientid]]
+    code: `$color[$getVar[color]]
+$author[Commands;$userAvatar[$clientid]]
 
-$description[1;[Invite]($getVar[inv]) • [Vote]($getVar[vote]) • [Support]($getVar[sserver])
-$addField[1;Server Setting;
+$description[[Invite]($getVar[inv]) • [Vote]($getVar[vote]) • [Support]($getVar[sserver])
+$addField[Server Setting;
 \`clear-filters\`, \`prefix\`
    ]
 
- $addField[1;Filters;
+ $addField[Filters;
 \`bassboost\`, \`3D\`, \`8D\`, \`16D\`, \`echo\`, \`gate\`, \`nightcore\`, \`phaser\`, \`pitch\`, \`purebass\`, \`slowed\`, \`space\`, \`speed\`, \`vaporwave\`
  ]
 
-$addField[1;Misc;
+$addField[Misc;
 \`help\`, \`invite\`, \`ping\`, \`stats\`, \`support\`, \`uptime\`, \`vote\`
    ]
  
-$addField[1;Music;
+$addField[Music;
 \`clearqueue\`, \`disconnect\`, \`join\`, \`loopqueue\`, \`loopsong\`, \`lyrics\`, \`nowplaying\`, \`pause\`, \`play\`, \`queue\`, \`remove\`, \`repeat\`, \`resume\`, \`search\`, \`seek\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop\`, \`volume\`
  ]]
 
-$footer[1;Requested by $userTag;$authorAvatar]
-$addTimeStamp[1]
+$footer[Requested by $userTag;$authorAvatar]
+$addTimeStamp
 $channelSendMessage[$getVar[messageChannel];$username#$discriminator[$authorID] (\`$authorID\`) used \`help\` command at $serverName (\`$guildID\`) in <#$channelID> (\`$channelID\`)]
    `
    }
