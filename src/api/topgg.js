d69ade0056a6b2bbae22a481cf5600edb1902c04
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-15 18:50:05 +0600

    Update topgg.js

--- src/api/topgg.js
+++ src/api/topgg.js
@@ -1,10 +1,11 @@
 module.exports = ({
     channel: "$getVar[readyChannel]",
-    type: "loop",
+    type: "loopCommand",
     every: 1800000,
     executeOnStartup: false,
     code: `
 $djsEval[const axios = require('axios')
+
 axios({
   method: 'post',
   url: 'https://top.gg/api/bots/$clientID/stats',
@@ -12,7 +13,9 @@ axios({
    server_count: client.guilds.cache.size
   }, headers: {
 'Authorization': '$getVar[TOPGG_AUTH]' 
+
 }
+
 })]
 `
 })
