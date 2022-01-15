8e5a51aa0342a7e9c2ba0b9fff50d04afc3e886a
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-15 18:44:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-const Aoijs = require('aoi.js')
+const Aoijs = require("aoi.js")
 const bot = new Aoijs.Bot(require("./config.json"))
 
+bot.loadCommands(`./src/`)
 
-//configs
-require('./player/lavalink')(bot)
 require('./handlers/variables')(bot)
-require('./handlers/callbacks')(bot)
+require('./handlers/events')(bot)
 require('./handlers/responses')(bot)
 require('./handlers/channels')(bot)
-require('./handlers/status')(bot)
+require('./handlers/activity')(bot)
 require('./handlers/secrets')(bot)
-
-const loader = new Aoijs.LoadCommands(bot)
-loader.load(bot.cmd, "./src/")
-
-loader.setColors({
-    walking: ["blink", "dim", "fgWhite"],
-    failedWalking: {
-        name: ["bright", "fgYellow", "underline"],
-
-        text: ["bright", "fgRed"]
-    },
-    typeError: {
-        command: ["bright", "fgYellow"],
-        type: ["fgYellow"],
-        text: ["bright", "fgRed"]
-    },
-    failLoad: {
-        command: ["bright", "fgMagenta"],
-        type: ["fgRed"],
-        text: ["bright", "fgRed"],
-    },
-    loaded: {
-        command: ["bright", "fgCyan"],
-        type: ["bright", "fgBlue"],
-        text: ["bright", "fgGreen"]
-    },
-})
-
