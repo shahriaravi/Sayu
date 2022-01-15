c3a8475e924361664e424dbf970bc5f352a65fa4
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-15 18:48:44 +0600

    Update ping.js

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,9 +1,9 @@
 module.exports = { 
     name: "ping",
     code: `
-👌 Connecting to bot's server...
-$editIn[1s;**👌 Pong!**
-> Websocket: \`$numberSeparator[$ping]ms\`, API: \`$numberSeparator[$messagePing]ms\`, Database: \`$numberSeparator[$dbPing]ms\`, Average: \`$numberSeparator[$truncate[$divide[$sum[$ping;$messagePing;$dbPing];3]]]ms\`]
+👌 Connecting to bot's sever...
+$editIn[1;**👌 Pong!**
+> Websocket: \`$numberSeparator[$ping]ms\`, API: \`$numberSeparator[$botPing]ms\`, Database: \`$numberSeparator[$dbPing]ms\`, Average: \`$numberSeparator[$truncate[$divide[$sum[$ping;$botPing;$dbPing];3]]]ms\`]
 $channelSendMessage[$getVar[messageChannel];$username#$discriminator[$authorID] (\`$authorID\`) used \`ping\` command at $serverName (\`$guildID\`) in <#$channelID> (\`$channelID\`)]
 `
 }
