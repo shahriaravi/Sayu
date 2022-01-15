c42e8d539d93f55e3ba7ea0ab63d320085fb6dac
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-15 18:50:39 +0600

    Update joinMessage.js

--- src/events/client/joinMessage.js
+++ src/events/client/joinMessage.js
@@ -1,16 +1,16 @@
 module.exports = {
-    type: "guildJoin",
+    type: "botJoinCommand",
       channel: "$systemChannelID",
       code: `
-      $color[1;$getVar[color]]
-      $thumbnail[1;$userAvatar[$clientID]]
-      $title[1;$getVar[name] - High Quality Music]
-      $description[1;
+      $color[$getVar[color]]
+      $thumbnail[$userAvatar[$clientID]]
+      $title[$getVar[name] - High Quality Music]
+      $description[
 Thank you for inviting me!
       
 Join a voice channel and \`$getServerVar[prefix]play\` a song.
 Type \`$getServerVar[prefix]help\` for the list of commands.
 [Invite]($getVar[inv]) | [Vote]($getVar[vote]) | [Support]($getVar[sserver])]
-      $footer[1;Latency: $ping ms | Uptime: $djsEval["$uptime[humanize]".split(" ").slice(0,-1).join(" ");yes]]
+      $footer[Latency: $ping ms | Uptime: $client[readytimestamp]]
       `
     }
