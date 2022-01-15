0aa0e70606adadbb08810ae52468818e4eb4dff5
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-15 18:51:21 +0600

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,14 +1,14 @@
 module.exports = [{
-    type: "ready",
+    type: "readyCommand",
     channel: "",
     code: `$log[
 [$formatDate[$dateStamp;LLLL]] › ✔  ready     Guilds $serverCount
-[$formatDate[$dateStamp;LLLL]] › ✔  ready     Members $allMembersCount
+[$formatDate[$dateStamp;LLLL]] › ✔  ready     Members $allMembersCount ready
 [$formatDate[$dateStamp;LLLL]] › ✔  ready     $userTag[$clientID] is ready
-[$formatDate[$dateStamp;LLLL]] › 🛰️  api       $ping ms]`
+[$formatDate[$dateStamp;LLLL]] › 🛰️  api      API listening at http://localhost:20101]`
 },
  {
-    type: "ready",
+    type: "readyCommand",
     channel: "$getVar[readyChannel]",
     code: `
     $userTag[$clientID] is online: $serverCount servers and $allMembersCount members.
