5c86467fb72f8ca72269b4f9560744a58f80d315
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-22 17:39:18 +0600

    Update stats.js

--- src/commands/misc/stats.js
+++ src/commands/misc/stats.js
@@ -5,7 +5,7 @@ module.exports = {
 $thumbnail[$userAvatar[$clientid]]
   $color[$getVar[color]]
   $title[Sayu's Statistics]
-  $description[**Uptime:** \`$client[readytimestamp]\`\n**Memory Usage:** \`$ram\`\n**CPU Usage:** \`$cpu%\`\n**Guilds:** \`$serverCount\`\n**Channels:** \`$allChannelsCount\`\n**Songs Played:** \`$getVar[played]\`\n**Discord.js Version:** \`v13.2.0\`\n**Node Version**: \`$nodeVersion\`\n**Sayu Version:** \`v3.7.0\`\n[Invite]($getVar[inv]) | [Vote]($getVar[vote]) | [Support]($getVar[sserver]) | [Creavite.co](https://creavite.co)]
+  $description[**Uptime:** \`$client[readytimestamp]\`\n**Memory Usage:** \`$round[$ram]\`\n**CPU Usage:** \`$round[$cpu]%\`\n**Guilds:** \`$serverCount\`\n**Channels:** \`$allChannelsCount\`\n**Songs Played:** \`$getVar[played]\`\n**Discord.js Version:** \`v13.2.0\`\n**Node Version**: \`$nodeVersion\`\n**Sayu Version:** \`v3.7.0\`\n[Invite]($getVar[inv]) | [Vote]($getVar[vote]) | [Support]($getVar[sserver]) | [Creavite.co](https://creavite.co)]
   $footer[Requested by $userTag;$authorAvatar]
   $addTimestamp
   `
