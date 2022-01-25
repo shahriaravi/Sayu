f6d63a09ddbfc9dbea02eac39686ac38753717f3
Shahriar Avi <79418237+shahriaravi@users.noreply.github.com>
2022-01-25 19:53:39 +0600

    Update variables.js

--- handlers/variables.js
+++ handlers/variables.js
@@ -3,7 +3,7 @@ module.exports = (bot) => {
     prefix: '.',
     "name": "Sayu",
     "color": "#E5D4B0",
-    inv: 'https://discord.com/oauth2/authorize?scope=bot+applications.commands&client_id=872844020416675892&permissions=37088600',
+    inv: 'https://discord.com/oauth2/authorize?client_id=872844020416675892&permissions=2180378176&redirect_uri=https://discord.gg/MscZrQq7eW&response_type=code&scope=bot%20applications.commands',
     vote: 'https://top.gg/bot/872844020416675892/vote',
     sserver: 'https://discord.gg/MscZrQq7eW',
     played: '',
